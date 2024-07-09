be2e36f4991b8f7b9f835b54445ad1008e3641e0
Aniket Jha <aniketjha@Anikets-Laptop.local>
2024-07-09 12:08:52 +0530

    New changes

--- src/components/Community/CommunityBanner.js
+++ src/components/Community/CommunityBanner.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Community.css"
 
 import House from "../../asset/house.png"
+import Image from 'next/image'
 
 const CommunityBanner = () => {
   return (
@@ -13,7 +14,7 @@ const CommunityBanner = () => {
                     <h1 className='xl:text-[5rem] font-[NeueBold] text-2xl inline  text-[#fff] leading-[4.5rem] font-bold text-center' >
                       A Social Club 
                     </h1>
-                    <p className='xl:my-3 font-[NeueRegular] text-sm text-center xl:text-3xl xl:w-[500px] text-[#fff] font-[600]' >Sign a lease and it's your <img src={House} className='inline w-6 xl:w-6'  /> too.</p>
+                    <p className='xl:my-3 font-[NeueRegular] text-sm text-center xl:text-3xl xl:w-[500px] text-[#fff] font-[600]' >Sign a lease and it's your <Image src={House} className='inline w-6 xl:w-6'  /> too.</p>
                     <div>
                        <a href="https://www.instagram.com/union_living/?hl=en" target='_blank' rel='noreferrer'  > <button className='border-2 xl:my-6 my-4 border-[#fff] bg-[#fff]  font-[NeueMedium] px-3 xl:px-6 xl:py-3 py-2 rounded-full text-xs xl:text-xl' >Join Our Community</button></a>
                     </div>
