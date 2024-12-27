a1ad2ab1577589361064613baa7b8b765812527f
Aniket Jha <aniketjha@Anikets-Laptop.local>
2024-12-27 13:26:39 +0530

    Union Mumbai

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -2,14 +2,12 @@
 import CommunityPage from './CommunityPage'
 
 export const metadata = {
- title: "Union Living",
+  title: "Union Living Mumbai | Luxuxy Coliving in Mumbai",
   description: "Experience a modern way of living in Mumbai, Pune, and Navi Mumbai with our coliving spaces! Fully furnished rooms and high-quality facilities designed for college students and office employees.",
-  keywords: ['Union Coliving','Union Living','Community Living in Mumbai', 'Community Living in Pune', 'Community Living in Thane','Community Living in Navi Mumbai', "Premium Co-living",'Best Community'],
-  
+  keywords: ['Union Living','Union Living Mumbai','Community Living in Mumbai', 'Community Living in Pune', 'Community Living in Thane','Community Living in Navi Mumbai', "Premium Co-living",'Best Community'],
 
- 
   openGraph: {
-    title: 'Union Living',
+    title: 'Union Living Mumbai | Luxuxy Coliving in Mumbai',
     description: 'Experience a modern way of living in Mumbai, Pune, and Navi Mumbai with our coliving spaces! Fully furnished rooms and high-quality facilities designed for college students and office employees.',
     url: 'https://unionliving.in',
     siteName: 'Union Living',
@@ -18,13 +16,12 @@ export const metadata = {
         url: 'https://firebasestorage.googleapis.com/v0/b/union-living.appspot.com/o/files%2Ffavicon.png?alt=media&token=63384a08-8830-4c2b-82b0-0355cf50bbff', // Must be an absolute URL
         width: 800,
         height: 600,
-        alt: 'Union Living',
       },
       {
         url: 'https://firebasestorage.googleapis.com/v0/b/union-living.appspot.com/o/files%2Ffavicon.png?alt=media&token=63384a08-8830-4c2b-82b0-0355cf50bbff', // Must be an absolute URL
         width: 1800,
         height: 1600,
-        alt: 'Union Living',
+        alt: 'My custom alt',
       },
     ],
   
@@ -32,8 +29,8 @@ export const metadata = {
  
   icons: {
     icon: 'https://firebasestorage.googleapis.com/v0/b/union-living.appspot.com/o/files%2Ffavicon.png?alt=media&token=63384a08-8830-4c2b-82b0-0355cf50bbff',
-    shortcut: './favicon.png',
-    apple: './favicon.png',
+    shortcut: 'https://firebasestorage.googleapis.com/v0/b/union-living.appspot.com/o/files%2Ffavicon.png?alt=media&token=63384a08-8830-4c2b-82b0-0355cf50bbff',
+    apple: 'https://firebasestorage.googleapis.com/v0/b/union-living.appspot.com/o/files%2Ffavicon.png?alt=media&token=63384a08-8830-4c2b-82b0-0355cf50bbff',
     other: {
       rel: 'apple-touch-icon-precomposed',
       url: 'https://firebasestorage.googleapis.com/v0/b/union-living.appspot.com/o/files%2Ffavicon.png?alt=media&token=63384a08-8830-4c2b-82b0-0355cf50bbff',
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@
 import Home from "./pages/Home";
 
 export const metadata = {
-  title: "Union Living",
+  title: "Union Living Mumbai | Luxuxy Coliving in Mumbai",
   description: "Experience a modern way of living in Mumbai, Pune, and Navi Mumbai with our coliving spaces! Fully furnished rooms and high-quality facilities designed for college students and office employees.",
-  keywords: ['Community Living in Mumbai', 'Community Living in Pune', 'Community Living in Thane','Community Living in Navi Mumbai', "Premium Co-living",'Best Community'],
+  keywords: ['Union Living','Union Living Mumbai','Community Living in Mumbai', 'Community Living in Pune', 'Community Living in Thane','Community Living in Navi Mumbai', "Premium Co-living",'Best Community'],
 
   openGraph: {
-    title: 'Union Living',
+    title: 'Union Living Mumbai | Luxuxy Coliving in Mumbai',
     description: 'Experience a modern way of living in Mumbai, Pune, and Navi Mumbai with our coliving spaces! Fully furnished rooms and high-quality facilities designed for college students and office employees.',
     url: 'https://unionliving.in',
     siteName: 'Union Living',
