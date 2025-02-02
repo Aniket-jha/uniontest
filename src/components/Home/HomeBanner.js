f1fe23bed2acd1ded25989c16e81324dc19ff379
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-02-03 01:32:17 +0530

    Added Meta Property

--- src/components/Home/HomeBanner.js
+++ src/components/Home/HomeBanner.js
@@ -12,7 +12,7 @@ const HomeBanner = ({homeContent}) => {
         </div>
       
         <div className='absolute left-0 bottom-0 flex items-center justify-center flex-col right-0 m-auto w-full text-center z-[3] top-[10px] lg:top-[-205px]'>
-            <h1 className='bg-[#ebe3d67d] my-2 font-[600]  xl:w-[30%] w-[60%] font-[Neue Haas Grotesk Display Pro] px-3 py-3 lg:py-6 lg:text-6xl' >Live <span className="italic" > Easy,</span> </h1>
+            <h2 className='bg-[#ebe3d67d] my-2 font-[600]  xl:w-[30%] w-[60%] font-[Neue Haas Grotesk Display Pro] px-3 py-3 lg:py-6 lg:text-6xl' >Live <span className="italic" > Easy,</span> </h2>
             <h2 className='bg-[#ebe3d67d]   font-[600]   xl:w-[30%] w-[60%] px-3 py-3 lg:py-6 lg:text-6xl'>Live <span className="italic" > United </span></h2>
             </div>
       
