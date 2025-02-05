786b1317655ce3238d3481fae0cbd53051291d74
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-02-05 10:13:29 +0530

    Added ID

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager } from '@next/third-parties/google'
 import Hotjar from "@hotjar/browser";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -44,14 +45,21 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  const siteId = 5292476;
-  const hotjarVersion = 6;
-
-      Hotjar.init(siteId, hotjarVersion);
+  
   
   return (
     <html lang="en">
-     
+     <Script id="HotJarAnalytics"
+     strategy="afterInteractive" 
+     dangerouslySetInnerHTML={`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5292476,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`}
+      />
 
       <link rel="icon" href="./favicon.png" sizes="any" />
       <link
