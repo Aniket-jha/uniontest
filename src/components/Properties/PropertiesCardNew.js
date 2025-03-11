fc24d1504cc3cca4231f3c15f7624b7876cf7519
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-03-12 00:25:00 +0530

    Changed Image

--- src/components/Properties/PropertiesCardNew.js
+++ src/components/Properties/PropertiesCardNew.js
@@ -33,10 +33,10 @@ const PropertiesCardNew = ({property}) => {
          </Link>
 
     <div className='my-0 flex items-center px-3 lg:px-3 py-3 justify-center flex-col text-center' >
-            <h4 className='font-[NeueMedium] text-[2rem]' >{property?.name}</h4>
-            <h4 className='font-[NeueRegular] text-[1.2rem]' >{property?.location}</h4>
+            <h4 className='font-[NeueMedium] text-[1.8rem]' >{property?.name}</h4>
+            <h4 className='font-[NeueRegular] text-[1rem]' >{property?.location}</h4>
             <p className="font-[NeueRegular]  py-1 text-[1.4rem] text-[#272727]" >Starting From: <span className='text-[#FC7451] font-[NeueMedium]' > {property?.price} </span></p>
-            <p className='font-[NeueRegular] mt-1 ' >{property?.content.slice(0,110)}...</p>
+            <p className='font-[NeueRegular] mt-1 ' >{property?.content.slice(0,95)}...</p>
             <div className='my-6' >
             <Link href={property.link} > <button className="uppercase flex items-center justify-center gap-3 rounded-lg shadow-md  font-[NeueMedium] ease-in duration-300  text-[14px] md:text-[18px]  hover:shadow-none hover:bg-[#272727] tracking-wider py-[6px] px-[24px] md:py-[8px] md:px-[24px] text-[#FFFEFC] bg-[#D64C27]" >Book Now  </button> </Link>
             </div>
