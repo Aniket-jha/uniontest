96ff2390f5db8ef5043f7e623368520ab254b2e2
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-03-17 18:53:54 +0530

    Added Video in Blog

--- src/components/Blogs/BlogContent.js
+++ src/components/Blogs/BlogContent.js
@@ -6,7 +6,11 @@ const SingleBlogContent = ({blogContent}) => {
     <div className='mx-3 my-12 xl:mx-36 xl:my-16 ' >
         <div className='flex items-start justify-start flex-col ' >
           
-           <img className=' w-full rounded-xl ' src={blogContent?.bannerImage} alt={blogContent?.imagealt ? blogContent?.imagealt : blogContent.name} />
+          { blogContent?.bannerImage ? <img className=' w-full rounded-xl ' src={blogContent?.bannerImage} alt={blogContent?.imagealt ? blogContent?.imagealt : blogContent.name} />
+          : (<video className=' z-[-1]  h-auto w-full' playsInline={true}   controls>
+          <source src={blogContent?.videourl} type="video/mp4" />
+      </video>)
+        }
          
           <div className='border-[1px] rounded-xl border-[#d0d0d0] mt-6  w-[100%] px-4 py-3 lg:px-12 lg:py-12  ' >
           {blogContent?.name && <h1  className='text-left font-[NeueBold] text-[#3c3c3c] my-6 text-[1.5rem] lg:text-[2.5rem] ' >{blogContent?.name} </h1>}
