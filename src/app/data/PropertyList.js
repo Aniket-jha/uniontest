76762b0e8d7819351d98c8233a10082c822700ef
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-04-07 18:46:26 +0530

    Removed Housemate

--- src/app/data/PropertyList.js
+++ src/app/data/PropertyList.js
@@ -317,38 +317,7 @@ export const propertyListData = [
               
     ]
     },
-    {
-        name:"Housemate",
-        coverImage:Housemate,
-        link:"/property/housemate-coliving-kharadi-pune",
-        propertyDescriptionTitle:"https://www.unionliving.in/#/form",
-        images:[
-            ChapterOneMainImage,ChapterOneOneImage,ChapterOneTwoImage,ChapterOneThreeImage,ChapterOneFourImage
-        ],
-        price:"Rs.30,000",
-        location:"kharadi, Pune",
-        content:"Introducing Housemate, the latest addition to the Union Living portfolio, coming soon in the thriving locality of Kharadi, Pune. ",
-        map:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.883075003392!2d72.8629156!3d19.1308596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c8a38409d307a53!2sCASA%20by%20Union%20Co-Living!5e0!3m2!1sen!2sin!4v1673692237980!5m2!1sen!2sin",
-        youtubeId:"sP_tmYSOWPE",
-        rooms:[
-            {
-                title:"Private",
-                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
-                image:ChapterOneTwoImage
-            },
-            {
-                title:"Twin",
-                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
-                image:ChapterOneCoverImage
-            },
-            {
-                title:"Triple",
-                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
-                image:ChapterOneOneImage
-            },
-            
-        ]
-    },
+   
    
     {
         name:"Casa",
