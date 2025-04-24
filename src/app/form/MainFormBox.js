d8b8785e6731e33f583609b44239b038d167f563
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-04-24 12:43:57 +0530

    Added New Property Input

--- src/app/form/MainFormBox.js
+++ src/app/form/MainFormBox.js
@@ -145,6 +145,7 @@ const MainFormBox = () => {
                         <option value="Chapter 1">Chapter 1, Belapur (Navi Mumbai)</option>
                         <option value="Abode">Abode, Dhankawadi (Pune)</option>
                         <option value="House of Balewadi">House of Balewadi, Balewadi (Pune)</option>
+                        <option value="Zynergy, Chembur (Mumbai)">Zynergy, Chembur (Mumbai)</option>
                              </select>
                     </div>
                     <div className='flex flex-col lg:flex-row gap-4 lg:gap-6 my-4 lg:my-6 w-full items-center justify-center' >
