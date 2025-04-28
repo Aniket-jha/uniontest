4a505fee0ec26791b3e221184901397f05ff1a69
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-04-28 12:54:27 +0530

    Changed Brochure

--- src/app/property/southside-coliving-mahalaxmi/SouthsideProperty.js
+++ src/app/property/southside-coliving-mahalaxmi/SouthsideProperty.js
@@ -72,12 +72,12 @@ const SouthsideProperty = () => {
        <Banner galleryImages={galleryImages} name="Southside: Premier CoLiving PG in Mahalaxmi, Mumbai" location="Mahalaxmi" images={images} />
      <Details subtitle="Mahalaxmi's Premier CoLiving Space" title="Discover a Lifestyle of Ease and Connection in Our Mahalaxmi CoLiving Space." description="Welcome to our co-living space in Mahalaxmi, a sanctuary where contemporary comfort meets a thriving community atmosphere. Situated in one of Mumbai’s most dynamic neighborhoods, our Luxury Co-Living Mumbai space offers an unparalleled living experience designed for modern urbanites. Whether you're a young professional, a digital nomad, or simply looking for a place that feels like home, we provide everything you need to live, work, and connect." />
        <Featured />
-       <Rooms rooms={rooms} brochure="https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/files%2FSouthside%20by%20Union%20Digital%20(2).pdf?alt=media&token=113cfd63-377c-4aa4-a362-01cdb240478a" />
+       <Rooms rooms={rooms} brochure="https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/_Southside%20by%20Union%202025.pdf?alt=media&token=222b300e-f7e8-4433-b171-b4e5e473ea66" />
        <WhatSection youtube="https://youtu.be/YukkDVVWgm0?si=OG8L4_vGW86mRxco" />
        <Location map="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15091.720798709732!2d72.8258839!3d18.9786898!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cfc462de8a1f%3A0x2a4ee6362fbea170!2sSouthside%20by%20Union%20Living!5e0!3m2!1sen!2sin!4v1714534828670!5m2!1sen!2sin" youtube="https://www.youtube.com/embed/YukkDVVWgm0?si=sp-H2rNbxc_dZbA4" />
        <Event />
       <TestimonialTwo />
-      <BottomNavConstant name="Southside"  price="Rs. 49,999" youtube="https://youtu.be/YukkDVVWgm0?si=OG8L4_vGW86mRxco" brochure="https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/files%2FSouthside%20by%20Union%20Digital%20(2).pdf?alt=media&token=113cfd63-377c-4aa4-a362-01cdb240478a"  />
+      <BottomNavConstant name="Southside"  price="Rs. 49,999" youtube="https://youtu.be/YukkDVVWgm0?si=OG8L4_vGW86mRxco" brochure="https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/_Southside%20by%20Union%202025.pdf?alt=media&token=222b300e-f7e8-4433-b171-b4e5e473ea66"  />
         <Footer />
         </div>
         </ThemeProvider>
