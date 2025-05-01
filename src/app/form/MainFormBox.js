8aae1fec5577927efc4e8d789fecdda317c4df4d
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-05-01 21:35:49 +0530

    Added Elements in Form

--- src/app/form/MainFormBox.js
+++ src/app/form/MainFormBox.js
@@ -145,7 +145,8 @@ const MainFormBox = () => {
                         <option value="Chapter 1">Chapter 1, Belapur (Navi Mumbai)</option>
                         <option value="Abode">Abode, Dhankawadi (Pune)</option>
                         <option value="House of Balewadi">House of Balewadi, Balewadi (Pune)</option>
-                        <option value="Zynergy, Chembur (Mumbai)">Zynergy, Chembur (Mumbai)</option>
+                        <option value="Zest, Chembur (Mumbai)">Zest, Chembur (Mumbai)</option>
+                        <option value="House of Balewadi, Pune">House of Balewadi, Pune</option>
                              </select>
                     </div>
                     <div className='flex flex-col lg:flex-row gap-4 lg:gap-6 my-4 lg:my-6 w-full items-center justify-center' >
