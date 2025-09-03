7fbaaa78b6ad70238cf9ab764564dd97ec486f82
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-09-03 16:19:25 +0530

    Changed Cover Images

--- src/components/Layout/Topbar.js
+++ src/components/Layout/Topbar.js
@@ -5,7 +5,7 @@ const Topbar = () => {
   return (
     <Link href="/property/zest-coliving-chembur" >
     <div className='w-full text-center bg-[#000000] text-white py-2' >
-        🚨 Introducing Zest, 5 Mins from BKC 🚨
+        🚨 House of KP, Coming Soon ! 🚨
     </div>
     </Link>
   )
