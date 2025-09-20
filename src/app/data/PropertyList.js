8dccf6b2cb534bb864aa13151c888ffbb2de9a44
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-09-20 20:07:35 +0530

    Changes Done

--- src/app/data/PropertyList.js
+++ src/app/data/PropertyList.js
@@ -935,6 +935,102 @@ export const activeProduct = [
             },
         ]
     },
+    {
+        name:"House of KP",
+        coverImage:HouseofKPCover,
+        link:"",
+        propertyDescriptionTitle:"https://www.unionliving.in/#/form",
+        images:[
+            ChapterOneMainImage,ChapterOneOneImage,ChapterOneTwoImage,ChapterOneThreeImage,ChapterOneFourImage
+        ],
+        price:"Coming Soon",
+        location:"Koregaon Park, Pune",
+        content:"Get ready for a fresh, innovative take on community living. House of KP is an upcoming union living property in the vibrant heart of Koregaon Park, designed for those who want more than just a place to stay – it’s a place to connect, collaborate, and thrive.",
+        map:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.883075003392!2d72.8629156!3d19.1308596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c8a38409d307a53!2sCASA%20by%20Union%20Co-Living!5e0!3m2!1sen!2sin!4v1673692237980!5m2!1sen!2sin",
+        youtubeId:"sP_tmYSOWPE",
+        rooms:[
+            {
+                title:"Private",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneTwoImage
+            },
+            {
+                title:"Twin",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneCoverImage
+            },
+            {
+                title:"Triple",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneOneImage
+            },
+            
+        ]
+    },
+    {
+        name:"House of Mundwa",
+        coverImage:HouseofMundwaCover,
+        link:"",
+        propertyDescriptionTitle:"https://www.unionliving.in/#/form",
+        images:[
+            ChapterOneMainImage,ChapterOneOneImage,ChapterOneTwoImage,ChapterOneThreeImage,ChapterOneFourImage
+        ],
+        price:"Coming Soon",
+        location:"Mundwa",
+        content:"A modern coliving space fostering community, collaboration, and comfort through shared living and workspaces.",
+        map:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.883075003392!2d72.8629156!3d19.1308596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c8a38409d307a53!2sCASA%20by%20Union%20Co-Living!5e0!3m2!1sen!2sin!4v1673692237980!5m2!1sen!2sin",
+        youtubeId:"sP_tmYSOWPE",
+        rooms:[
+            {
+                title:"Private",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneTwoImage
+            },
+            {
+                title:"Twin",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneCoverImage
+            },
+            {
+                title:"Triple",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneOneImage
+            },
+            
+        ]
+    },
+    {
+        name:"Elv-8",
+        coverImage:Elv8Cover,
+        link:"",
+        propertyDescriptionTitle:"https://www.unionliving.in/#/form",
+        images:[
+            ChapterOneMainImage,ChapterOneOneImage,ChapterOneTwoImage,ChapterOneThreeImage,ChapterOneFourImage
+        ],
+        price:"Coming Soon",
+        location:"Viman Nagar",
+        content:"A vibrant coliving hub merging creativity, connection, and convenience with shared spaces and collaborative energy.",
+        map:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.883075003392!2d72.8629156!3d19.1308596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c8a38409d307a53!2sCASA%20by%20Union%20Co-Living!5e0!3m2!1sen!2sin!4v1673692237980!5m2!1sen!2sin",
+        youtubeId:"sP_tmYSOWPE",
+        rooms:[
+            {
+                title:"Private",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneTwoImage
+            },
+            {
+                title:"Twin",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneCoverImage
+            },
+            {
+                title:"Triple",
+                content:"Modern twin-sharing rooms in a vibrant co-living space, fostering community connections.",
+                image:ChapterOneOneImage
+            },
+            
+        ]
+    },
    
 ]
 
--- src/components/Layout/Topbar.js
+++ src/components/Layout/Topbar.js
@@ -2,12 +2,46 @@ import Link from 'next/link'
 import React from 'react'
 
 const Topbar = () => {
+  const items = [
+    '😍  Gurugram coming soon  😍',
+    '🔥  Baner 2.0 coming soon  🔥',
+    '🚨  House of KP, Coming Soon 🚨' ,
+   
+  ]
   return (
-    <Link href="/property/zest-coliving-chembur" >
-    <div className='w-full text-center bg-[#000000] text-white py-2' >
-        🚨 Gurugram coming soon || 
-        Baner 2.0 Coming soon  🚨
-    </div>
+    <Link href="/property/zest-coliving-chembur">
+      <div className="w-full bg-[#000000] text-white py-2 marquee cursor-pointer">
+        <div className="marquee__track">
+          <div className="marquee__content px-4">
+            {items.map((text, idx) => (
+              <span key={`a-${idx}`} className="text-sm mx-0 sm:text-base">
+                {text}
+              </span>
+            ))}
+          </div>
+          <div className="marquee__content px-4" aria-hidden="true">
+            {items.map((text, idx) => (
+              <span key={`b-${idx}`} className="text-sm sm:text-base">
+                {text}
+              </span>
+            ))}
+          </div>
+          <div className="marquee__content px-4" aria-hidden="true">
+            {items.map((text, idx) => (
+              <span key={`b-${idx}`} className="text-sm sm:text-base">
+                {text}
+              </span>
+            ))}
+          </div>
+          <div className="marquee__content px-4" aria-hidden="true">
+            {items.map((text, idx) => (
+              <span key={`b-${idx}`} className="text-sm sm:text-base">
+                {text}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
     </Link>
   )
 }
