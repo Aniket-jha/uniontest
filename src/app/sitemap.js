f60796a7c3edd2a81d9f4c52702cb618d3fc894d
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-09-26 01:38:41 +0530

    Added KP in Sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -126,6 +126,12 @@ export default async function sitemap() {
       priority: 0.8,
     },
     {
+      url: 'https://unionliving.in/property/house-of-kp-koregoan-park/',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
       url: 'https://unionliving.in/community/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
