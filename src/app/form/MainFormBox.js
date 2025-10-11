732896dd5470a1d6b89a4acc0661314c42bee944
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-10-11 21:46:34 +0530

    KP Changes

--- src/app/form/MainFormBox.js
+++ src/app/form/MainFormBox.js
@@ -172,6 +172,7 @@ const MainFormBox = ({redirectMainSection}) => {
                         <option value="Hamlet">Hamlet, Baner (Pune)</option>
                         <option value="House of KP">House of KP, Koregoan Park (Pune)</option>
                         <option value="House of KP">Nova, Gurugram</option>
+                        <option value="House of KP">Eden, Baner (Pune)</option>
                         {/* <option value="Casa">Casa, Andheri (Mumbai)</option>
                         <option value="Chapter 1">Chapter 1, Belapur (Navi Mumbai)</option>
                         <option value="Abode">Abode, Dhankawadi (Pune)</option> */}
