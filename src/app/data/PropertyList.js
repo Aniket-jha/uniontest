398bad5f0e74187d26a783b30aed461520e1170b
Aniket Jha <aniketjha@Anikets-Laptop.local>
2026-01-05 12:41:01 +0530

    Changed Properly Location

--- src/app/data/PropertyList.js
+++ src/app/data/PropertyList.js
@@ -1134,7 +1134,7 @@ export const activeProduct = [
             ChapterOneMainImage,ChapterOneOneImage,ChapterOneTwoImage,ChapterOneThreeImage,ChapterOneFourImage
         ],
         price:"Coming Soon",
-        location:"Viman Nagar",
+        location:"Baner, Pune",
         content:"A vibrant coliving hub merging creativity, connection, and convenience with shared spaces and collaborative energy.",
         map:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.883075003392!2d72.8629156!3d19.1308596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c8a38409d307a53!2sCASA%20by%20Union%20Co-Living!5e0!3m2!1sen!2sin!4v1673692237980!5m2!1sen!2sin",
         youtubeId:"sP_tmYSOWPE",
