cfb83f9948b7cd65f7179a8617c8592cbbb93d06
Sushil Jadhav <sushiljadhav407@gmail.com>
2026-02-03 16:41:53 +0530

    remove opening text

--- src/app/data/PropertyList.js
+++ src/app/data/PropertyList.js
@@ -123,7 +123,7 @@ export const propertyListData = [
             ChapterOneMainImage,ChapterOneOneImage,ChapterOneTwoImage,ChapterOneThreeImage,ChapterOneFourImage
         ],
         price:"Rs. 25,000",
-        location:"Cyber City, Gurugram (Opening 15th November)",
+        location:"Cyber City, Gurugram ",
         content:"Modern coliving with premium amenities, vibrant community, near Cyber City.",
         map:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.883075003392!2d72.8629156!3d19.1308596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c8a38409d307a53!2sCASA%20by%20Union%20Co-Living!5e0!3m2!1sen!2sin!4v1673692237980!5m2!1sen!2sin",
         youtubeId:"sP_tmYSOWPE",
@@ -717,7 +717,7 @@ export const activeProduct = [
             ChapterOneMainImage,ChapterOneOneImage,ChapterOneTwoImage,ChapterOneThreeImage,ChapterOneFourImage
         ],
         price:"Rs. 25,000",
-        location:"Cyber City, Gurugram (Opening 15th November)",
+        location:"Cyber City, Gurugram",
         content:"Modern coliving with premium amenities, vibrant community, near Cyber City.",
         map:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.883075003392!2d72.8629156!3d19.1308596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c8a38409d307a53!2sCASA%20by%20Union%20Co-Living!5e0!3m2!1sen!2sin!4v1673692237980!5m2!1sen!2sin",
         youtubeId:"sP_tmYSOWPE",
